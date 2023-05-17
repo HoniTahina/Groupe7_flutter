6931d66e7c9e59c63125e92bbbded71a95bc46b6
HoniTahina <ricatahina@gmail.com>
2023-05-17 22:27:11 +0000

    aaa

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
   runApp(const MyApp());
 }
 
